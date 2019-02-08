fcb87054219f2e3e3ea21c71c20840cc6c266a1a
Samuel Birch <sam@kurious.agency>
2019-02-08 14:50:28 +0000

    delete icon & loading

--- src/assetbundles/ordersnew/dist/js/Orders.js
+++ src/assetbundles/ordersnew/dist/js/Orders.js
@@ -261,114 +261,116 @@ $(document).ready(function() {
 		var $tab = $('#orderDetailsTab');
 
 		if ($('#order-completionStatus')[0]) {
-
 			$('#main-form').removeAttr('data-confirm-unload');
-			
+
 			$tab.append($('<div id="order-extras" class="flex"><div></div><div><input type="text" name="couponCode" class="text" value="" placeholder="Coupon Code"> <a class="btn submit" data-id="update">Update</a></div></div>'));
 
 			$tab.find('[data-id="update"]').on('click', function(e) {
 				e.preventDefault();
-
+				$('.order-details').addClass('loading');
 				$('input[name=action]').val('commerce/cart/update-cart');
-				
+
 				$('#main-form')
 					.append($('<input type="hidden" name="redirect" value="' + $('#main-form').attr('data-saveshortcut-redirect') + '">'))
 					.submit();
 			});
 
+			$tab.find('table thead tr').append($('<th scope="col" class="thin"></th>'));
+
 			$('.infoRow').each(function() {
 				var $row = $(this),
 					$qty = $row.find('td[data-title="Qty"]'),
 					$input = $('<input type="number" min="0" name="lineItems[' + $row.attr('data-id') + '][qty]" value="' + Number($qty.text()) + '">');
+				$('<td class="thin"><a class="delete icon" data-id="' + $row.attr('data-id') + '" title="Delete" role="button"></a></td>').appendTo($row);
 
 				$qty.empty().append($input);
 			});
 
 			$.ajax({
-				type: "GET",
+				type: 'GET',
 				dataType: 'json',
 				url: '',
 				data: {
-					'action' : 'commerce-admin-orders/orders/get-admin-variants',
+					action: 'commerce-admin-orders/orders/get-admin-variants'
 				},
 				success: function(data) {
 					$tab.append(data.html);
 
 					Craft.elementIndex = Craft.createElementIndex('kuriousagency\\commerce\\adminorders\\elements\\AdminVariant', $('#admin-variants'), {
-						context:        'index',
-						storageKey:     'elementindex.kuriousagency\\commerce\\adminorders\\elements\\AdminVariant',
-						criteria:       Craft.defaultIndexCriteria
+						context: 'index',
+						storageKey: 'elementindex.kuriousagency\\commerce\\adminorders\\elements\\AdminVariant',
+						criteria: Craft.defaultIndexCriteria
 					});
 				}
 			});
 
 			$.ajax({
-				type: "POST",
+				type: 'POST',
 				dataType: 'json',
 				headers: {
-					"X-CSRF-Token" : $('[name="CRAFT_CSRF_TOKEN"]').val(),
+					'X-CSRF-Token': $('[name="CRAFT_CSRF_TOKEN"]').val()
 				},
 				url: '',
 				data: {
-					'action' : 'commerce-admin-orders/orders/get-order-number-by-id',
-					'orderId': $('input[name="orderId"]').val(),
+					action: 'commerce-admin-orders/orders/get-order-number-by-id',
+					orderId: $('input[name="orderId"]').val()
 				},
 				success: function(data) {
 					$('#main-form').append($('<input type="hidden" name="orderNumber" value="' + data.orderNumber + '">'));
 				}
 			});
 
-
-			$tab.on('click','.atc',function(e) {
+			$tab.on('click', '.atc', function(e) {
 				e.preventDefault();
-	
-				var $this = $(this);
-					purchasableId = $this.attr('data-id'),
-					qty = $tab.find('[name="adminOrderQty['+purchasableId+']"]').val();
-	
-				$('.order-details').addClass('loading');
-	
-				if(qty > 0) {
 
+				var $this = $(this);
+				(purchasableId = $this.attr('data-id')), (qty = $tab.find('[name="adminOrderQty[' + purchasableId + ']"]').val());
+
+				$('.order-details').addClass('loading');
+
+				if (qty > 0) {
 					$.ajax({
-						type: "POST",
+						type: 'POST',
 						dataType: 'json',
 						headers: {
-							"X-CSRF-Token" : $('[name="CRAFT_CSRF_TOKEN"]').val(),
+							'X-CSRF-Token': $('[name="CRAFT_CSRF_TOKEN"]').val()
 						},
 						url: '',
 						data: {
-							'action' : 'commerce-admin-orders/orders/add-to-cart',
-							'purchasableId': purchasableId,
-							'adminOrderQty': qty,
-							'orderId': $('input[name="orderId"]').val(),
+							action: 'commerce-admin-orders/orders/add-to-cart',
+							purchasableId: purchasableId,
+							adminOrderQty: qty,
+							orderId: $('input[name="orderId"]').val()
 						},
 						success: function(data) {
 							$('.order-details').empty();
 							$('.order-details').html(data.html);
 							$('.order-details').removeClass('loading');
-	
+
 							var notification = '<div class="notification notice">Product added to cart</div>';
 							$('#notifications-wrapper #notifications').html(notification);
-							$('#notifications-wrapper #notifications .notification').delay(2000).fadeOut(400);
-	
-							$.each($('.tableRowInfo'), function () {
+							$('#notifications-wrapper #notifications .notification')
+								.delay(2000)
+								.fadeOut(400);
+
+							$.each($('.tableRowInfo'), function() {
 								new Craft.Commerce.TableRowAdditionalInfoIcon(this);
 							});
 						}
 					});
 				}
-	
-			})
+			});
 
-			$tab.on('click','.infoRow a.delete',function(e) {
+			$tab.on('click', '.infoRow a.delete', function(e) {
 				e.preventDefault();
 				console.log($(this).data('id'));
-				$tab.find('[name="lineItems['+$(this).data('id')+'][qty]"]').val('0');
+				$('.order-details').addClass('loading');
+				$tab.find('[name="lineItems[' + $(this).data('id') + '][qty]"]').val('0');
 				// $('<input type="hidden" name="lineItems['+$(this).data('id')+'][qty]" value="0">').appendTo($tab);
-				$('#main-form').append($('<input type="hidden" name="redirect" value="' + $('#main-form').attr('data-saveshortcut-redirect') + '">')).submit();
-			})		
-
+				$('#main-form')
+					.append($('<input type="hidden" name="redirect" value="' + $('#main-form').attr('data-saveshortcut-redirect') + '">'))
+					.submit();
+			});
 		}
 	}
 });
